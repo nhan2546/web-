484053686f6b05a66c5216bb2ae45625014f6601
TuanMinh685 <letrantuanminh4@gmail,com>
2025-10-04 11:07:31 +0700

    sửa được ít

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php include 'pages/header.php'; ?>
+<?php include 'GiaoDien/trang/bo_cuc/dau_trang.php'; ?>
     <main>
         <h2>Chào Mừng Đến Shop!</h2>
         <p>Danh sách sản phẩm mới nhất sẻ hiển thị ở đây.</p>
     </main>
-    <?php include 'pages/footer.php'; ?>
+    <?php include 'GiaoDien/trang/bo_cuc/chan_trang.php'; ?>
